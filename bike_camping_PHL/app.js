0af55d89ae7ac79165716830b69d8b00c4574dda
Travis Southard <tsouthard88@gmail.com>
2020-06-16 18:52:17 -0400

    Started on list of campgrounds with active-api ran into access-control-allow-origin error. Need to go cook.

--- bike_camping_PHL/app.js
+++ bike_camping_PHL/app.js
@@ -1,5 +1,22 @@
+const campgroundApiKey = "&api_key=ZJNTTHN8M976Q987YP48VZKW";
+const activeApiKey = "&api_key=H8XCTF6FVEGX87PUZXAWQ28Y"
+const baseURL = "http://api.amp.active.com/camping/campgrounds?";
+const testURL = "http://api.amp.active.com/camping/campgrounds?pstate=CO&siteType=2001&expwith=1&amenity=4005&pets=3010";
 
-(() => { // On page load
+console.log("This works");
 
-    
-})
+$(() => { // On page load
+    console.log("But does this?");
+    const getCampgrounds = () => {
+        $.ajax({
+            type: "GET",
+            url: baseURL + "pstate=PA" + campgroundApiKey,
+        }).done((campgroundData) => {
+            console.log(campgroundData);
+            console.log("Just in case");
+        }), (error) => {
+            console.log(error);
+        }
+    }
+    getCampgrounds();
+});
