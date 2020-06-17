61e9dd6561488925ce001aa85d9d5d55a7b1f0fb
Travis Southard <tsouthard88@gmail.com>
2020-06-17 10:01:43 -0400

    Adjusted campground api key to proper casing from all caps. Still getting the CORS error, submitted an email to Active.com to try to resolve

--- bike_camping_PHL/app.js
+++ bike_camping_PHL/app.js
@@ -1,9 +1,7 @@
-const campgroundApiKey = "&api_key=ZJNTTHN8M976Q987YP48VZKW";
-const activeApiKey = "&api_key=H8XCTF6FVEGX87PUZXAWQ28Y"
+const campgroundApiKey = "&api_key=zjntthn8m976q987yp48vzkw";
+const activeApiKey = "&api_key=H8XCTF6FVEGX87PUZXAWQ28Y";
 const baseURL = "http://api.amp.active.com/camping/campgrounds?";
 const testURL = "http://api.amp.active.com/camping/campgrounds?pstate=CO&siteType=2001&expwith=1&amenity=4005&pets=3010";
-
-console.log("This works");
 
 $(() => { // On page load
     console.log("But does this?");
