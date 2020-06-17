37760d8052c142e54c8f62b30789f8fd1cfc06b5
Travis Southard <tsouthard88@gmail.com>
2020-06-17 11:21:01 -0400

    Trying to see if I get a CORB error from github pages and if my error is a local error

--- bike_camping_PHL/app.js
+++ bike_camping_PHL/app.js
@@ -1,14 +1,17 @@
 const campgroundApiKey = "&api_key=zjntthn8m976q987yp48vzkw";
 const activeApiKey = "&api_key=H8XCTF6FVEGX87PUZXAWQ28Y";
-const baseURL = "http://api.amp.active.com/camping/campgrounds?";
-const testURL = "http://api.amp.active.com/camping/campgrounds?pstate=CO&siteType=2001&expwith=1&amenity=4005&pets=3010";
+const baseURL = "https://api.amp.active.com/camping/campgrounds?";
+const testURL = "https://api.amp.active.com/camping/campgrounds?pstate=CO&siteType=2001&expwith=1&amenity=4005&pets=3010";
+let philaLat = 39.9528;
+let philaLong = -75.1635;
+const coordQuery = `landmarkName=true&landmarkLat=${philaLat}&landmarkLong=${philaLong}`
 
 $(() => { // On page load
-    console.log("But does this?");
     const getCampgrounds = () => {
         $.ajax({
-            type: "GET",
-            url: "https://api.amp.active.com/camping/campgrounds/?pstate=PA&Maxpeople=4&api_key=zjntthn8m976q987yp48vzkw",
+            method: "GET",
+            url: baseURL + coordQuery + campgroundApiKey,
+            dataType: "jsonp",
         }).done((campgroundData) => {
             console.log(campgroundData);
             console.log("Just in case");
