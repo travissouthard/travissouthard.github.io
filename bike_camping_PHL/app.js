200546104f8296887c9462bb85ddc9915d921167
Travis Southard <tsouthard88@gmail.com>
2020-06-22 11:58:07 -0400

    Added some more styles: hover for checklist li elements, weather card width, and autofocus for the input box

--- bike_camping_PHL/app.js
+++ bike_camping_PHL/app.js
@@ -4,7 +4,7 @@ const baseChecklist = {
     Bike: ["bike", "lock", "lights", "spare tubes", "patch kit", "pump/CO2", "multitool", "tire levers"],
     Kitchen: ["stove", "stove fuel", "pot/pan", "spatula", "plate/bowl", "spork", "mug", "lighter", "plastic bags", "camp snacks", "ride snacks", "coffee/tea", "lunch 1", "dinner", "breakfast", "lunch 2", "sponge", "dish soap",],
     Body: ["cycling shirt", "cycling socks", "sportsbra", "phone", "wallet", "keys", "camp shirt", "underwear", "bra", "hoodie", "towel", "bandana", "glasses", "sunglasses", "headlamp", "phone charger"],
-    Toiletries: ["first aid kit", "allergy meds", "bugspray", "body soap", "toothbrush", "toothpaste", "contact case & solution", "chamois butter"],
+    Toiletries: ["first aid kit", "allergy meds", "bugspray", "body soap", "toothbrush", "toothpaste", "contact case & solution", "chamois butter", "intimacy kit"],
     Custom: [],
 };
 const hotChecklist = {
