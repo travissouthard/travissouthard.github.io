82403bb70da9b98d3403cfba9f838610a83b7923
Travis Southard <tsouthard88@gmail.com>
2020-09-05 17:29:23 -0400

    Created react frontend and wrote out the beginning of a readme

---
+++ games/dungeon/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <h1>Dungeon Game</h1>
+        <div id="board"></div>
+      </>
+    )
+  }
+}
+
+export default App;
