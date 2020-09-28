9cd12a876d649791c45754e5d791e7c007554861
Travis Southard <tsouthard88@gmail.com>
2020-09-28 15:49:23 -0400

    Added the ball elements and basic randomizer

--- games/8ball/app.js
+++ games/8ball/app.js
@@ -1,1 +1,36 @@
-console.log("The app works")
+let responses = [
+    "It is certain",
+    "It is decidedly so",
+    "Without a doubt",
+    "Yes - definitely",
+    "You may rely on it",
+    "As I see it, yes",
+    "Most likely",
+    "Outlook good",
+    "Yes",
+    "Signs point to yes",
+    "Reply hazy, try again",
+    "Ask again later",
+    "Better not tell you now",
+    "Cannot predict now",
+    "Concentrate and ask again",
+    "Don't count on it",
+    "My reply is no",
+    "My sources say no",
+    "Outlook not so good",
+    "Very doubtful",
+]
+
+let counter = 0
+
+$(() => {
+    const $button = $("button")
+
+    const getRandomAnswer = () => {
+        $("#answer").text(responses[Math.floor(Math.random() * 20)])
+    }
+
+    $button.click(getRandomAnswer)
+
+    getRandomAnswer()
+})
