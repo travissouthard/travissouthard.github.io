be23ab1bbc0625bb16f6c2bf834d0446681b32cc
Travis Southard <travis@Traviss-MBP.fios-router.home>
2022-02-19 17:41:25 -0500

    Refactor projects into data file
    Also updated the about section at the bottom

---
+++ data.js
@@ -0,0 +1,49 @@
+let projectData = [
+    {
+        "title": "Expungement Petition Generator",
+        "siteLink": "https://www.plsephilly.org/",
+        "codeLink": "",
+        "imagePath": "./assets/images/plse-expungement.png",
+        "description": "",
+        "altText": "Screenshot of the Expungement Petition Generator",
+        "public": false
+    },
+    {
+        "title": "Bike Camping PHL",
+        "siteLink": "./bike_camping_PHL/index.html",
+        "codeLink": "https://github.com/travissouthard/travissouthard.github.io/tree/master/bike_camping_PHL",
+        "imagePath": "./assets/images/bike_camping_phl.png",
+        "description": "",
+        "altText": "Screenshot of my Bike Camping PHL checklist app.",
+        "public": true
+    },
+    {
+        "title": "Civilian Crash Report",
+        "siteLink": "https://crashreportphl.herokuapp.com/",
+        "codeLink": "https://github.com/travissouthard/project2",
+        "imagePath": "./assets/images/crash_report_phl.png",
+        "description": "",
+        "altText": "Screenshot of my Crash Report PHL app.",
+        "public": true
+    },
+    {
+        "title": "Minesweeper Clone",
+        "siteLink": "./games/minesweeper/index.html",
+        "codeLink": "https://github.com/travissouthard/travissouthard.github.io/tree/master/games/minesweeper",
+        "imagePath": "./assets/images/minesweeper.png",
+        "description": "",
+        "altText": "Screenshot of my Minesweeper game.",
+        "public": true
+    },
+    {
+        "title": "Gravity Play",
+        "siteLink": "https://editor.p5js.org/travissouthard/full/Fq_vEfA8O",
+        "codeLink": "https://editor.p5js.org/travissouthard/sketches/Fq_vEfA8O",
+        "imagePath": "./assets/images/gravity-play.png",
+        "description": "",
+        "altText": "Screenshot of small colorful planets orbiting each other in a gravity simulation.",
+        "public": true
+    }
+]
+
+let skills = []
