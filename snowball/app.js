14567c9d528883e6c3c39386186c2d1c63281a6e
travissouthard <tsouthard88@gmail.com>
2023-01-02 18:14:26 -0500

    Add debt object and calculate payments

--- snowball/app.js
+++ snowball/app.js
@@ -1,30 +1,59 @@
-const formRowData = [
+const formRowItems = [
     {name: "debt-name", type: "text", placeholder: "Name of debt", label: "Name:"},
     {name: "debt-amount", type: "number", placeholder: "Amount Remaining", label: "Amount:", min: "0"},
     {name: "debt-minimum", type: "number", placeholder: "Minimum payment", label: "Minimum:", min: "0"},
     {name: "debt-rate", type: "number", placeholder: "Interest rate", label: "Interest:", min: "0", max: "100"},
 ]
 
+class Debt {
+    constructor({name, amount, minimum, rate}) {
+        this.name = name || ""
+        this.amount = amount || 0
+        this.minimum = minimum || 0
+        this.rate = rate || 0
+        this.paydown = this.calculatePayments(amount, minimum, rate, [amount])
+        this.paymentseft = this.paydown.length
+    }
+
+    roundCents = (num) => {
+        return parseFloat(num.toFixed(2))
+    }
+
+    calculatePayments = (remaining, min, rate, payments = []) => {
+        const interest = remaining * ((rate/100)/12)
+        let newRemaining = remaining + interest
+        if (newRemaining < min && newRemaining - min <= 0) {
+            return payments
+        }
+
+        newRemaining = newRemaining - min
+        payments.push(this.roundCents(newRemaining))
+        return this.calculatePayments(newRemaining, min, rate, payments)
+    }
+}
+
+let rowNum = 0
 const addDebtRow = () => {
     const container = document.getElementById("row-container")
     const blankDebtRow = document.createElement("div")
     blankDebtRow.className = "form-row"
 
-    for (let row of formRowData) {
+    for (let item of formRowItems) {
         const label = document.createElement("label")
-        const labelText = document.createTextNode(row.label)
-        label.for = row.name
+        const labelText = document.createTextNode(item.label)
+        const itemName = `${item.name}-${rowNum}`
+        label.for = itemName
         label.appendChild(labelText)
 
         const input = document.createElement("input")
-        input.name = row.name
-        input.type = row.type
-        input.placeholder = row.placeholder
-        if (row.min !== undefined) {
-            input.min = row.min
+        input.name = itemName
+        input.type = item.type
+        input.placeholder = item.placeholder
+        if (item.min !== undefined) {
+            input.min = item.min
         }
-        if  (row.max !== undefined) {
-            input.max = row.max
+        if  (item.max !== undefined) {
+            input.max = item.max
         }
 
         blankDebtRow.appendChild(label)
@@ -39,6 +68,7 @@ const addDebtRow = () => {
     }
 
     container.appendChild(blankDebtRow)
+    rowNum++
 }
 
 const addDebtButton = document.getElementById("add-debt")
@@ -46,6 +76,29 @@ addDebtButton.addEventListener("click", (e) => {
     e.preventDefault()
     e.stopPropagation()
     addDebtRow()
+})
+
+const createDebtObjects = (elements) => {
+    const debts = []
+    let count = 0
+    while (elements[`debt-name-${count}`] !== undefined) {
+        let debtData = {
+            name: elements[`debt-name-${count}`].value,
+            amount: elements[`debt-amount-${count}`].valueAsNumber,
+            minimum: elements[`debt-minimum-${count}`].valueAsNumber,
+            rate: elements[`debt-rate-${count}`].valueAsNumber
+        }
+        debts.push(new Debt(debtData))
+        count++
+    }
+    return debts
+}
+
+const submitButton = document.getElementById("submit")
+submitButton.addEventListener("click", (e) => {
+    e.preventDefault()
+    e.stopPropagation()
+    console.log(createDebtObjects(document.getElementById("debt-form").elements))
 })
 
 window.addEventListener("load", (e) => {
