7e22139e079956d9336647be281a943f777c061f
travissouthard <tsouthard88@gmail.com>
2023-04-22 10:45:11 -0400

    Add blog page and generator

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
+const assembleBlogPost = (blog) => {
+  const pubDate = new Date(blog.lastUpdated);
+  const $blogPost = $("<article></article>").attr("class", "blog-post");
+  const $title = $(`<h3>${blog.title}</h3>`);
+  const $dateLine = $(`<h5>Last updated: ${pubDate.toDateString()}</h5>`);
+  const blogHtml = $.parseHTML(blog.description);
+
+  $blogPost.append($title);
+  $blogPost.append($dateLine);
+  $blogPost.append(blogHtml);
+
+  return $blogPost;
+};
+
 const assembleProjectCard = (project) => {
   const $card = $("<article></article>").attr("class", "project-card");
   const $projectImage = $("<a></a>").attr({
@@ -42,6 +56,43 @@ const fillOutSections = (key, value) => {
   }
 };
 
+const fillOutBlogSection = (blogs, choice = null) => {
+  if (choice === null) choice = blogs.length - 1;
+  const orderedBlogs = sortArrayByDate(blogs).reverse();
+  const $blogNav = $("<ul></ul>").attr("class", "blog-nav");
+  const $first = $("<li>&lt;&lt;</li>").click(() =>
+    fillOutBlogSection(blogs, 0)
+  );
+  const $prev = $("<li>&lt;</li>").click(() =>
+    fillOutBlogSection(blogs, choice - 1)
+  );
+  const $next = $("<li>&gt;</li>").click(() =>
+    fillOutBlogSection(blogs, choice + 1)
+  );
+  const $last = $("<li>&gt;&gt;</li>").click(() =>
+    fillOutBlogSection(blogs, blogs.length - 1)
+  );
+
+  if (choice > 0) {
+    $blogNav.append($first);
+    $blogNav.append($prev);
+  } else {
+    $blogNav.append($("<li></li><li></li>"));
+  }
+  if (choice + 1 < blogs.length) {
+    $blogNav.append($next);
+    $blogNav.append($last);
+  } else {
+    $blogNav.append($("<li></li><li></li>"));
+  }
+
+  const $blogSection = $("#blog");
+  $blogSection.empty();
+  const $chosenPost = assembleBlogPost(orderedBlogs[choice]);
+  $blogSection.append($chosenPost);
+  $blogSection.append($blogNav);
+};
+
 $(() => {
   const sectionData = {
     "#projects": data.projects,
@@ -51,4 +102,5 @@ $(() => {
   Object.entries(sectionData).map(([key, value]) =>
     fillOutSections(key, value)
   );
+  fillOutBlogSection(data.blogs);
 });
